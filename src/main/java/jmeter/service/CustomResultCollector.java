b5dfaffab7731bedd9f33a85d5bb2ca12dc1cb04
vardanaivazian <vardan.aivazian@eyesoftinc.com>
2019-12-25 18:24:35 +0400

    add custom result collector

---
+++ src/main/java/jmeter/service/CustomResultCollector.java
@@ -0,0 +1,61 @@
+package jmeter.service;
+
+import jmeter.bean.Endpoint;
+import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jmeter.reporters.Summariser;
+import org.apache.jmeter.samplers.SampleEvent;
+import org.apache.jmeter.samplers.SampleResult;
+
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomResultCollector extends ResultCollector {
+
+    private static final Logger LOGGER = Logger.getLogger( CustomResultCollector.class.getName() );
+    private transient EndpointService endpointService;
+
+    public CustomResultCollector( Summariser summer ) {
+        super( summer );
+        endpointService = new EndpointServiceImpl();
+    }
+
+    public CustomResultCollector() {
+        super();
+    }
+
+    @Override
+    public void sampleOccurred( SampleEvent event ) {
+        super.sampleOccurred( event );
+        SampleResult sampleResult = event.getResult();
+        if( sampleResult.isSuccessful() ) {
+            LOGGER.log( Level.INFO,"result: {0}", sampleResult );
+            LOGGER.log(Level.INFO, "Response time in milliseconds: {0}", sampleResult.getTime() );
+
+            Endpoint endpoint = endpointService.findEndpoint( sampleResult.getSampleLabel() ); // found endpoint by id 
+
+            if( endpoint == null ) {
+                LOGGER.log( Level.SEVERE, "Endpoint not fount by id: {0}", sampleResult.getSampleLabel() );
+            }
+
+            LOGGER.info( "HttpSample success id: " + sampleResult.getSampleLabel() + ", responseCode: " + sampleResult.getResponseCode() + ", responseMessage: " + sampleResult.getResponseMessage() );
+        }
+        else {
+            LOGGER.warning( "HttpSample failed id: " + sampleResult.getSampleLabel() + ", responseCode: " + sampleResult.getResponseCode() + ", responseMessage: " + sampleResult.getResponseMessage() );
+        }
+    }
+
+    @Override
+    public boolean equals( Object o ) {
+        if( this == o ) return true;
+        if( o == null || getClass() != o.getClass() ) return false;
+        if( !super.equals( o ) ) return false;
+        CustomResultCollector that = ( CustomResultCollector ) o;
+        return Objects.equals( endpointService, that.endpointService );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( super.hashCode(), endpointService );
+    }
+}
